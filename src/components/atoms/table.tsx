bdd893d4c4ad09f570f128180b0f938035673527
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-05-18 18:15:13 -0300

    feat: table component

--- src/components/atoms/table.tsx
+++ src/components/atoms/table.tsx
@@ -1,19 +1,20 @@
 interface TableProps extends React.HTMLAttributes<HTMLTableElement> {
   className?: string;
   headers?: string[] | string;
+  headerClassName?: string;
 }
 
-const Table: React.FC<TableProps> = ({ children, className, headers }) => {
+const Table: React.FC<TableProps> = ({ children, className, headers, headerClassName }) => {
   // A função mapeia uma array de strings e as retorna dentro de uma tag "th".
   const mapHeadersArray = (input: string[]) => {
-    return input.map((title) => <th key={title}>{title}</th>);
+    return input.map((title) => <th key={title} className={headerClassName}>{title}</th>);
   };
 
   // A função retrna o(s) título(s) dentro de uma tag "th".
   const headersToTableData = (input: string[] | string) => {
     if (typeof input === "object" && input.length > 1)
       return mapHeadersArray(input);
-    else return <th>{input}</th>;
+    else return <th className={headerClassName}>{input}</th>;
   };
 
   // Caso o parâmetro "headers" tenha um valor aceito, a função renderiza o cabeçalho da tabela.
