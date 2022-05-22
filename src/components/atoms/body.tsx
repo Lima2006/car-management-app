c30094d3bdab78e80247af58e20191d4240cfa52
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-05-22 16:19:47 -0300

    fix: body custom classname

--- src/components/atoms/body.tsx
+++ src/components/atoms/body.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Column from "./column";
 
-const Body: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ children }) => (
-  <Column className="flex-1 px-8 py-4">{children}</Column>
+const Body: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ children, className }) => (
+  <Column className={["flex-1 px-8 py-4", className].join(" ")}>{children}</Column>
 );
 
 export default Body;
