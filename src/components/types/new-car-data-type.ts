2d335e18010ed43959e38864534aa7253b7f5e46
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-05-26 16:08:21 -0300

    feat: new car data type

---
+++ src/components/types/new-car-data-type.ts
@@ -0,0 +1,10 @@
+import BrandDataType from "./brand-data-type"
+
+type NewCarDataType = {
+  name: string,
+	plate: string,
+	color: string,
+	brand: BrandDataType,
+}
+
+export default NewCarDataType
