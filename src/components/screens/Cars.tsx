2d40c3608275180ce3e80ba0c9d1cc3962c7ed35
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-05-26 16:05:26 -0300

    feat: added label to input component

--- src/components/screens/Cars.tsx
+++ src/components/screens/Cars.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Body from "../atoms/body";
-import CarTableRows from "../atoms/car-table-rows";
+import CarTableRows from "../molecules/car-table-rows";
 import Input from "../atoms/input";
 import Row from "../atoms/row";
 import Selector from "../atoms/selector";
@@ -9,13 +9,17 @@ import Webpage from "../atoms/webpage";
 import Navbar from "../molecules/navbar";
 import BrandDataType from "../types/brand-data-type";
 import CarDataType from "../types/car-data-type";
-import PlusIcon from "../assets/icons/plus-icon.svg"
+import PlusIcon from "../assets/icons/plus-icon.svg";
 import LinkButton from "../atoms/link-button";
 import { useQuery, UseQueryResult } from "react-query";
 import { getCarsList } from "../../services/getCarsList";
+import Title from "../atoms/title";
 
 const Cars: React.FC = () => {
-  const { status, error, data: cars }: UseQueryResult<CarDataType[], Error> = useQuery<CarDataType[], Error>("cars", getCarsList)
+  const { data: cars }: UseQueryResult<CarDataType[], Error> = useQuery<
+    CarDataType[],
+    Error
+  >("cars", getCarsList);
   const brandList: BrandDataType[] = [
     { name: "Fiat", id: 0 },
     { name: "Alfa Romeo", id: 1 },
@@ -25,34 +29,43 @@ const Cars: React.FC = () => {
     <Webpage title="Carros">
       <Navbar />
       <Body className="space-y-4">
-        <Row className="justify-between">
-          <h1>Carros</h1>
-          <LinkButton href="/carro/novo">
+        <Row className="justify-between items-center">
+          <Title>Carros</Title>
+          <LinkButton href="/carros/novo">
             <PlusIcon width="24px" />
             <span>Novo Carro</span>
           </LinkButton>
         </Row>
         <Row className="space-x-4">
-          <label htmlFor="plateFilter">Filtrar por carro</label>
           <Input
             id="plateFilter"
             type="text"
             value={plateFilter}
             onChange={(e) => setPlateFilter(e.target["value"])}
+            label="Filtrar por placa"
           />
           <Selector
             name="selecionar-marcas"
-            id="brand-selector"
-            placeholder="Todas"
+            id="select-brand"
+            label="Filtrar por marca"
+            defaultValue="all"
           >
-            
+            <option value="all">Todas</option>
+            {brandList.map((brand: BrandDataType) => (
+              <option key={brand.id} value={brand.id}>
+                {brand.name}
+              </option>
+            ))}
           </Selector>
         </Row>
         <Table
-          className={{table: "border", header: ["w-1/4", "w-1/4", "w-auto", "w-48"]}}
+          className={{
+            table: "border",
+            header: ["w-1/4", "w-1/4", "w-auto", "w-48"],
+          }}
           headers={["Placa", "Cor", "Marca", "Ações"]}
         >
-            <CarTableRows cars={cars} className={{rows: "odd:bg-gray-100"}} />
+          <CarTableRows cars={cars} className={{ rows: "odd:bg-gray-100" }} />
         </Table>
       </Body>
     </Webpage>
