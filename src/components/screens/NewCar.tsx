dcc59bf223493ad9210c59abab37a01d2944a2d4
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-05-28 17:49:45 -0300

    feat: add toasts for error and success

--- src/components/screens/NewCar.tsx
+++ src/components/screens/NewCar.tsx
@@ -1,9 +1,12 @@
 import { useRouter } from "next/router";
+import { useContext } from "react";
 import { useMutation, UseMutationResult } from "react-query";
+import { toast } from "react-toastify";
 import { postCar } from "../../services/postCar";
 import Body from "../atoms/body";
 import Title from "../atoms/title";
 import Webpage from "../atoms/webpage";
+import showToastContext from "../contexts/show-toast-context";
 import CarForm from "../molecules/car-form";
 import Navbar from "../molecules/navbar";
 import CarDataType from "../types/car-data-type";
@@ -11,13 +14,15 @@ import NewCarDataType from "../types/new-car-data-type";
 
 const NewCar: React.FC = () => {
   const { push } = useRouter();
+  const { successToast, errorToast } = useContext(showToastContext);
   const { mutate }: UseMutationResult<CarDataType, Error, NewCarDataType> =
-    useMutation<CarDataType, Error, NewCarDataType>(
-      (data) => postCar(data),
-      {
-        onSuccess: () => push("/carros")
-      }
-    );
+    useMutation<CarDataType, Error, NewCarDataType>((data) => postCar(data), {
+      onSuccess: () => {
+        successToast("Carro cadastrado com sucesso!");
+        push("/carros");
+      },
+      onError: (err) => errorToast(err.message),
+    });
   return (
     <Webpage title="Novo Carro">
       <Navbar />
