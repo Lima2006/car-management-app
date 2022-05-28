41038bcc7a5efa29cd9d96e227652c1276664fe5
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-05-28 14:06:09 -0300

    fix: table error Fix div inside table

--- src/components/molecules/car-table-rows.tsx
+++ src/components/molecules/car-table-rows.tsx
@@ -34,57 +34,59 @@ const CarTableRows: React.FC<CarTableRowsProps> = ({
   );
 
   return (
-    <>
-      <ToastContainer
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover={false}
-      />
-      <tbody className={className.body}>
-        {cars?.map((car: CarDataType) => {
-          return (
-            <tr key={car.id} className={className.rows || ""}>
-              <td>{car.plate}</td>
-              <td>{car.color}</td>
-              <td>{car.brand.name}</td>
-              <td className="space-x-2 flex py-2">
-                <Button
-                  className="bg-blue-400 flex flex-row"
-                  onClick={() => console.log("Editar " + car.id)}
-                >
-                  <EditIcon width="24px" />
-                  Editar
-                </Button>
-                <Button
-                  className="bg-red-500 flex flex-row"
-                  onClick={() => setShowDeleteModal(car.id)}
-                >
-                  <DeleteIcon width="24px" />
-                  Excluir
-                </Button>
-                <ConfirmPopup
-                  showing={showDeleteModal === car.id}
-                  confirmed={() => mutate(car.id)}
-                  onClose={() => setShowDeleteModal(undefined)}
-                  title={`Tem certeza que deseja excluir este carro?`}
-                >
-                  <Column className="items-center overflow-hidden">
-                    <CarInformationCard car={car} />
-                    Essa ação não pode ser desfeita.
-                  </Column>
-                </ConfirmPopup>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </>
+    <tbody className={className.body}>
+      {cars?.map((car: CarDataType) => {
+        return (
+          <tr key={car.id} className={className.rows || ""}>
+            <td>{car.plate}</td>
+            <td>{car.color}</td>
+            <td>{car.brand.name}</td>
+            <td className="space-x-2 flex py-2">
+              <Button
+                className="bg-blue-400 flex flex-row"
+                onClick={() => console.log("Editar " + car.id)}
+              >
+                <EditIcon width="24px" />
+                Editar
+              </Button>
+              <Button
+                className="bg-red-500 flex flex-row"
+                onClick={() => setShowDeleteModal(car.id)}
+              >
+                <DeleteIcon width="24px" />
+                Excluir
+              </Button>
+              <ConfirmPopup
+                showing={showDeleteModal === car.id}
+                confirmed={() => mutate(car.id)}
+                onClose={() => setShowDeleteModal(undefined)}
+                title={`Tem certeza que deseja excluir este carro?`}
+              >
+                <Column className="items-center overflow-hidden">
+                  <CarInformationCard car={car} />
+                  Essa ação não pode ser desfeita.
+                </Column>
+              </ConfirmPopup>
+            </td>
+          </tr>
+        );
+      })}
+      <tr className="absolute">
+        <td>
+          <ToastContainer
+            position="top-center"
+            autoClose={3000}
+            hideProgressBar
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover={false}
+          />
+        </td>
+      </tr>
+    </tbody>
   );
 };
 
