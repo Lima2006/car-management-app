c4d4a9cff8d9d4bab8176bd18e195129684c3d98
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-05-28 20:49:50 -0300

    feat: brand table

--- src/components/screens/Brands.tsx
+++ src/components/screens/Brands.tsx
@@ -6,19 +6,43 @@ import PlusIcon from "../assets/icons/plus-icon.svg";
 import LinkButton from "../atoms/link-button";
 import Title from "../atoms/title";
 import "react-toastify/dist/ReactToastify.css";
+import BrandTableRows from "../molecules/brand-table-rows";
+import { useQuery } from "react-query";
+import { getBrands } from "../../services/getBrands";
+import Table from "../atoms/table";
+import BrandDataType from "../types/brand-data-type";
+import { useContext } from "react";
+import showToastContext from "../contexts/show-toast-context";
 
 const Brands: React.FC = () => {
+  // === Toast Context ===
+  const { errorToast } = useContext(showToastContext);
+  // === Query ===
+  const { data: brands } = useQuery<BrandDataType[], Error>(
+    "brands",
+    () => getBrands(),
+    {
+      onError: (err) => errorToast(err.message),
+    }
+  );
+
   return (
     <Webpage title="Marcas">
       <Navbar />
       <Body className="space-y-4">
         <Row className="justify-between items-center">
           <Title>Marcas</Title>
-          <LinkButton href="/carros/novo">
+          <LinkButton href="/marcas/novo">
             <PlusIcon width="24px" />
             <span>Nova Marca</span>
           </LinkButton>
         </Row>
+        <Table headers={["Nome", "Ações"]} className={{ header: ["", "w-48"] }}>
+          <BrandTableRows
+            brands={brands}
+            className={{ rows: "odd:bg-gray-100" }}
+          />
+        </Table>
       </Body>
     </Webpage>
   );
