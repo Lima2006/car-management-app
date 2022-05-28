7603b065fb5ce34e14c97f0e25bb352b53a22874
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-05-27 23:04:33 -0300

    fix: typescript

--- src/services/getCarsList.ts
+++ src/services/getCarsList.ts
@@ -1,6 +1,11 @@
+import CarDataType from "../components/types/car-data-type";
 import { api } from "../libs/api";
 
-export const getCarsList = async () => {
+type ParamsType = void;
+type ResponseType = CarDataType[];
+type ServiceType = (params?: ParamsType) => Promise<ResponseType>;
+
+export const getCarsList: ServiceType = async () => {
   const res = await api.get("/car");
-  return res.data
-}
+  return res.data;
+};
