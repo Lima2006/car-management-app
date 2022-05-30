f575b03bd5750d25677f2a814fd2a3c16710e93b
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-05-29 22:48:47 -0300

    feat: error screen

--- src/components/screens/Brands.tsx
+++ src/components/screens/Brands.tsx
@@ -11,38 +11,53 @@ import { useQuery } from "react-query";
 import { getBrands } from "../../services/getBrands";
 import Table from "../atoms/table";
 import BrandDataType from "../types/brand-data-type";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import showToastContext from "../contexts/show-toast-context";
+import ErrorScreen from "./error-screen";
+import Column from "../atoms/column";
 
 const Brands: React.FC = () => {
   // === Toast Context ===
   const { errorToast } = useContext(showToastContext);
+  // === Catch error ===
+  const [errorData, setErrorData] = useState(undefined);
   // === Query ===
-  const { data: brands } = useQuery<BrandDataType[], Error>(
-    "brands",
-    () => getBrands(),
-    {
-      onError: (err) => errorToast(err.message),
-    }
-  );
+  const {
+    data: brands,
+    isSuccess,
+    isError,
+  } = useQuery<BrandDataType[], Error>("brands", () => getBrands(), {
+    onError: (err) => {
+      errorToast(err.message);
+      setErrorData(err);
+    },
+  });
 
   return (
     <Webpage title="Marcas">
       <Navbar />
-      <Body className="space-y-4">
-        <Row className="justify-between items-center">
-          <Title>Marcas</Title>
-          <LinkButton href="/marcas/novo">
-            <PlusIcon width="24px" />
-            <span>Nova Marca</span>
-          </LinkButton>
-        </Row>
-        <Table headers={["Nome", "Ações"]} className={{ header: ["", "w-48"] }}>
-          <BrandTableRows
-            brands={brands}
-            className={{ rows: "odd:bg-gray-100" }}
-          />
-        </Table>
+      <Body>
+        {isError && <ErrorScreen error={errorData} />}
+        {isSuccess && (
+          <Column className="space-y-4">
+            <Row className="justify-between items-center">
+              <Title>Marcas</Title>
+              <LinkButton href="/marcas/novo" className="bg-gray-100 border shadow">
+                <PlusIcon width="24px" />
+                <span>Nova Marca</span>
+              </LinkButton>
+            </Row>
+            <Table
+              headers={["Nome", "Ações"]}
+              className={{ header: ["", "w-48"] }}
+            >
+              <BrandTableRows
+                brands={brands}
+                className={{ rows: "odd:bg-gray-100" }}
+              />
+            </Table>
+          </Column>
+        )}
       </Body>
     </Webpage>
   );
