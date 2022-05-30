201f7d1d599e9e2bb60b1ab40a5b2399d84b231c
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-05-29 23:19:41 -0300

    feat: outline none when readonly

--- src/components/atoms/input.tsx
+++ src/components/atoms/input.tsx
@@ -30,7 +30,11 @@ const Input: React.FC<InputProps> = ({
     <input
       id={id}
       type={type}
-      className={["border shadow rounded-full px-2 h-8", className.input].join(" ")}
+      className={[
+        "border shadow rounded-full px-2 h-8",
+        onChange === undefined ? "outline-none" : "",
+        className.input,
+      ].join(" ")}
       value={value}
       onChange={(e) => onChange(e)}
       placeholder={placeholder}
