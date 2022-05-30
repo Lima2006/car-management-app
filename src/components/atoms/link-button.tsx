7c61f37c1b451b18789f48d94309226cb21d7f10
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-05-29 22:26:32 -0300

    feat: visual modifications Add shadow, hover and more

--- src/components/atoms/link-button.tsx
+++ src/components/atoms/link-button.tsx
@@ -1,14 +1,32 @@
 import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
 
-interface LinkButtonProps extends LinkProps {
-  children: any
+interface LinkButtonProps {
+  href: string;
+  children: any;
   className?: string;
 }
 
-const LinkButton: React.FC<LinkButtonProps> = ({ href, children, className }) => (
-  <Link href={href}>
-    <div className={["bg-gray-200 px-2 py-1 rounded-lg flex flex-row cursor-pointer items-center h-min", className].join(" ")}>{children}</div>
-  </Link>
-);
+const LinkButton: React.FC<LinkButtonProps> = ({
+  href,
+  children,
+  className,
+}) => {
+  // === Next router ===
+  const { asPath } = useRouter();
+  return (
+    <Link href={href}>
+      <div
+        className={[
+          "hover:bg-white px-2 py-1 rounded-lg flex flex-row cursor-pointer items-center h-min",
+          asPath.includes(href) ? "bg-white shadow rounded-md" : "",
+          className,
+        ].join(" ")}
+      >
+        {children}
+      </div>
+    </Link>
+  );
+};
 
 export default LinkButton;
